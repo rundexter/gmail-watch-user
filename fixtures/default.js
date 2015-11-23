6d93e334bd63a3e1e9ffe9c8d206d9e0086e5dc0
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-23 19:51:53 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,8 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                userId: 'gmail_mail@gmail.com',
+                topicName: 'projects/my-project-id/topics/my-topic-id'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+var _ = require('lodash'),
+  google = require('googleapis'),
+  OAuth2 = google.auth.OAuth2;
+
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+
+        if(!step.input('userId').first()) {
+
+            this.fail('A userId input variable is required for this module');
+        }
+
+        if(!dexter.environment('access_token')) {
+
+            this.fail('A access_token environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +23,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+
+        google.options({ auth: oauth2Client });
+        google.gmail('v1').users.watch({
+            auth: oauth2Client,
+            userId: step.input('userId').first(),
+            resource: _.pick(step.inputs(), ['labelIds', 'labelFilterAction', 'topicName'])
+        }, function (err, message) {
+            console.log({
+                auth: oauth2Client,
+                userId: step.input('userId').first(),
+                resource: _.pick(step.inputs(), ['labelIds', 'labelFilterAction', 'topicName'])
+            });
+            err? this.fail(err) : this.complete(message);
+        }.bind(this));
+
     }
 };
